dae170c1c8267a1960f3c9c9e73d3f6115253b27
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2016-07-06 17:02:29 -0600

    added components

---
+++ src/components/PhotoGridComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//PhotoGrid.less');
+
+class PhotoGridComponent extends React.Component {
+  render() {
+    return (
+      <div className="photogrid-component">
+        Please edit src/components///PhotoGridComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+PhotoGridComponent.displayName = 'PhotoGridComponent';
+
+// Uncomment properties you need
+// PhotoGridComponent.propTypes = {};
+// PhotoGridComponent.defaultProps = {};
+
+export default PhotoGridComponent;
---
+++ src/components/SingleComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Single.less');
+
+class SingleComponent extends React.Component {
+  render() {
+    return (
+      <div className="single-component">
+        Please edit src/components///SingleComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+SingleComponent.displayName = 'SingleComponent';
+
+// Uncomment properties you need
+// SingleComponent.propTypes = {};
+// SingleComponent.defaultProps = {};
+
+export default SingleComponent;
